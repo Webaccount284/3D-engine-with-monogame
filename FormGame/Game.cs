24401d72619d0ce177feaeb52d1af8f120d9954e
David Stall <davidstall312@gmail.com>
2025-08-05 10:15:02 -0700

    added render data class, broke chunk loading

--- FormGame/Game.cs
+++ FormGame/Game.cs
@@ -10,7 +10,31 @@ using System.Security.Cryptography;
 
 namespace FormGame
 {
-
+    /// <summary>
+    /// Class for any game object defined by programmer
+    /// </summary>
+    internal class RenderData
+    {
+        public static int vertexCount = 0;
+        public static bool dirty = true;
+        public string id;
+        public Vector3 position;
+        public VertexPositionTexture[] data;
+        public RenderData(string id, Vector3 position, VertexPositionTexture[] data)
+        {
+            this.id = id;
+            this.position = position;
+            this.data = data;
+            vertexCount += data.Length;
+        }
+        public void ChangePosition(Vector3 transform)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i].Position += transform;
+            }
+        }
+    }
     public class Game : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -28,15 +52,15 @@ namespace FormGame
         BasicEffect basicEffect;
 
         // World
-        World world = new World(new Vector2(32, 32));
+        World world = new World(new Vector2(6, 6));
+        List<RenderData> renderData = new List<RenderData>();
 
         //Geometric info
         public VertexPositionTexture[] triangleVertices;
         Microsoft.Xna.Framework.Graphics.VertexBuffer vertexBuffer;
-        int triangleCount = 1;
+        int triangleCount = 0;
         Texture2D tileSheet;
-        List<Vector2> loadedChunks = new List<Vector2>();
-        List<VertexPositionTexture> chunkMeshes = new List<VertexPositionTexture>();
+
         bool reloadChunkMesh = true;
         Vector2 chunkLoc;
         int chunkRenderDistance = 2; // creates a square around player. A render distance of 2 means that a 3x3 grid of chunks is rendered. A render distance of 4 means 7x7 grid of chunks is rendered. (renderdist * 2) - 1
@@ -58,7 +82,6 @@ namespace FormGame
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up); 
             worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
 
-            
             // BasicEffect
             basicEffect = new BasicEffect(GraphicsDevice);
             basicEffect.Alpha = 1f;
@@ -69,12 +92,9 @@ namespace FormGame
             basicEffect.LightingEnabled = false;
 
             // World
-            chunkMeshes.AddRange(world.GetChunkMesh(new Vector2(0, 0), new Vector3(0, 0, 0)));
-            chunkMeshes.AddRange(world.GetChunkMesh(new Vector2(1, 0), new Vector3(0, 0, 0)));
-            loadedChunks.Add(new Vector2(0, 0)); 
-            loadedChunks.Add(new Vector2(1, 0));
-            // triangleVertices = LoadBlocks();
-            vertexBuffer = new Microsoft.Xna.Framework.Graphics.VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), triangleCount * 3, BufferUsage.WriteOnly);
+            RenderData chunk = new RenderData("Chunk", new Vector3(0, 0, 0) /*the chunk position*/, world.GetChunkMesh(new Vector2(0, 0), new Vector3(0, 0, 0)));
+            renderData.Add(chunk);
+            vertexBuffer = new Microsoft.Xna.Framework.Graphics.VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), (int)triangleCount * 3, BufferUsage.WriteOnly);
             // vertexBuffer.SetData<VertexPositionTexture>(triangleVertices);
         }
         protected override void LoadContent()
@@ -176,11 +196,23 @@ namespace FormGame
             camTarget = new Vector3((float)(camPosition.X + Math.Sin(camAngle.X * (Math.PI / 180))), (float)(camPosition.Y + Math.Sin(camAngle.Y * (Math.PI / 180))), (float)(camPosition.Z + Math.Cos(camAngle.X * (Math.PI / 180))));
             viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
 
-            triangleVertices = chunkMeshes.ToArray();
-            triangleCount = chunkMeshes.Count / 3;
-
+            if (RenderData.dirty == true)// checks if the data is dirty
+            {
+                RenderData.dirty = false;
+                List<VertexPositionTexture> worldMesh = new List<VertexPositionTexture>();
+                for (int i = 0; i < renderData.Count; i++)
+                {
+                    worldMesh.AddRange(renderData[i].data);
+                }
+                triangleCount = worldMesh.Count;
+                triangleVertices = new VertexPositionTexture[triangleCount];
+                triangleVertices = worldMesh.ToArray();
+                vertexBuffer.Dispose();
+                vertexBuffer = new Microsoft.Xna.Framework.Graphics.VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), triangleCount * 3, BufferUsage.WriteOnly);
+                vertexBuffer.SetData<VertexPositionTexture>(triangleVertices);
+            }
+            /*
             Vector2[] chunks = CalculateLoadedChunks();
-            
             if (loadedChunks.Count == chunks.Length)
             {
                 for (int i = 0; i < chunks.Length - 1; i++)
@@ -210,7 +242,7 @@ namespace FormGame
                 vertexBuffer.Dispose();
                 vertexBuffer = new Microsoft.Xna.Framework.Graphics.VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), triangleCount * 3, BufferUsage.WriteOnly);
                 vertexBuffer.SetData<VertexPositionTexture>(triangleVertices);
-            }
+            }*/
             base.Update(gameTime);
         }
         private Vector2[] CalculateLoadedChunks()
@@ -253,7 +285,7 @@ namespace FormGame
             foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, triangleCount * 3);
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, triangleCount / 3);
             }
             base.Draw(gameTime);
         }
